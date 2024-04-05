10b96feb3656a8a77c9fa4e44d2d01335b0a0e69
Gleb <glebgrin99@bk.ru>
2024-04-05 10:43:10 +0300

    Initeal commit

---
+++ task_1.py
@@ -0,0 +1,43 @@
+class FlatIterator:
+
+    def __init__(self, list_of_lists):
+        self.main_list = list_of_lists
+
+
+    def __iter__(self):
+        self.outer_list_cursor = 0
+        self.inner_list_cursor = -1
+        return self
+
+
+    def __next__(self):
+        self.inner_list_cursor += 1
+        if self.inner_list_cursor == len(self.main_list[self.outer_list_cursor]):
+            self.outer_list_cursor += 1
+            self.inner_list_cursor = 0
+        if self.outer_list_cursor == len(self.main_list):
+            raise StopIteration
+
+        return self.main_list[self.outer_list_cursor][self.inner_list_cursor]
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
